6e218265b3e41b7c100831667e2bbef1b04d63b7
Alexandre Velloso <4320811+AlexandreVelloso@users.noreply.github.com>
2018-06-28 18:56:31 -0300

    Ainda não funciona

--- 2018-1/Como fazer um chat online usando NodeJS/Codigos/Chat/app.js
+++ 2018-1/Como fazer um chat online usando NodeJS/Codigos/Chat/app.js
@@ -1,14 +1,30 @@
-const fs = require('fs');
-const express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
+/*
+Rota, ou seja, quando o servidor receber a url localhost:porta/
+ele vai fazer essa acao. Se ele receber uma outra url ele vai dar erro
+pois eu nao trato ela
+*/
 app.get('/', function(req,res){
-    fs.readFile('./chat.html', function(err, data){
-        if( err ) throw err;
-        res.end( data );
-    });
+    res.sendFile(__dirname + '/chat.html');
 });
 
-app.listen(3000, function(){
-  console.log('servidor rodando na porta 3000');
+
+io.on('connection', function(socket){
+   socket.on('chat message', function(msg){
+      
+      const splited = msg.split('|');
+      
+      const nome = splited[0];
+      const mensagem = splited.slice( 1, splited.length );
+      
+      io.emit('chat message', nome+": "+mensagem);
+   });
+});
+
+http.listen(port, function(){
+   console.log('servidor rodando na porta '+port);
 });
